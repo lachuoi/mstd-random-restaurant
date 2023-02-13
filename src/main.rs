0112603e881b3072e70ad2a8466e0c05df2b3798
Seungjin Kim <seungjin@seungjin.net>
2023-02-13 17:02:16 +0900

    MX(Mexico) removed from weighted country list

--- src/main.rs
+++ src/main.rs
@@ -42,7 +42,7 @@ struct Geopoint {
 fn get_random_city(r: &mut Restaurant, g: Vec<Geopoint>) {
 
     let mut weighted_points: Vec<Geopoint> = Vec::new();
-    let weighted_countries = vec!["DE","FR","ES","IT","TW","TH","VN","MX","PT","KR"];
+    let weighted_countries = vec!["DE","FR","ES","IT","TW","TH","VN","PT","KR"];
 
     let mg = g.iter().filter( |&g|
         g.population.unwrap_or(0) > 25000_i64
